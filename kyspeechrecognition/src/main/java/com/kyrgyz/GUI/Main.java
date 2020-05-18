107994bfa94fcec2f2fc8e50b401c7fbaf8d88e3
begemotkyz <abdygany.isaev@iaau.edu.kg>
2020-05-18 14:23:12 +0600

    update

---
+++ kyspeechrecognition/src/main/java/com/kyrgyz/GUI/Main.java
@@ -0,0 +1,55 @@
+
+package com.kyrgyz.GUI;
+
+import java.io.IOException;
+
+/**
+ *
+ * @author ABDYGANY
+ */
+public class Main extends javax.swing.JFrame {
+
+
+    public Main() {
+        initComponents();
+    }
+
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 570, Short.MAX_VALUE)
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 420, Short.MAX_VALUE)
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) throws IOException {
+
+        /* Create and display the form */
+        DemoApp content = new DemoApp();
+        Main window = new Main();
+        window.setContentPane(content);
+        window.setVisible(true);
+
+
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    // End of variables declaration//GEN-END:variables
+
+}
